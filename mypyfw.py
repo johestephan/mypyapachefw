954675abe6a649593d226c1e32e5d55233bd27cd
root <root@johest.de>
2014-10-02 22:10:33 +0200

    Licence, logging, optparser, help

--- mypyfw.py
+++ mypyfw.py
@@ -2,15 +2,33 @@
 # 
 # V1-0 by Joerg Stephan <joerg DOT stephan AT owasp DOT org>
 # https://github.com/johestephan/mypyapachefw.git
+# Copyright (c) 2014 Joerg Stephan under BSD licence
+#
 
 import iptc
 import sys
 import re
 import datetime
+from optparse import OptionParser
 
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename",
+                  help="write report to FILE", metavar="FILE")
+parser.add_option("-i", "--ippos", dest="IPpos", type="int",
+		  help="adjust IP position, default is 0", metavar="IPPOSITION")
+(options, args) = parser.parse_args()
+
+if (options.filename is None): 
+    options.filename = "/var/log/mypyfw.log"
+if (options.IPpos is None):
+    options.IPpos = 1
+
+logf = open(options.filename,'a')
+sys.stdout = logf
 recent = list()
 for line in sys.stdin:
-    IP = line.split()[1] # May need to be adjust, default 0 should work, combined is 1
+    IP = line.split()[options.IPpos] # May need to be adjust, default 0 should work, combined is 1
     Client = line.split('"')[-2]
     m = re.search('Wget|Python|sqlmap|curl',Client) # related services
     i = re.search('127.0.0.1|87.179.164.194',IP) # Whitelabeld IP's
@@ -25,4 +43,6 @@ for line in sys.stdin:
                 rule.create_target("DROP")
                 chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
                 chain.insert_rule(rule)
+logf.close()
+sys.stdout = sys.__stdout__
 
