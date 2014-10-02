6dc4b0fa3249d2c5a02a6fe2183132c392d4ecfe
root <root@johest.de>
2014-10-02 21:24:33 +0200

    fixes, logging prepared

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ README - My Python Apache Firewall
 
 ## Console run
 * cat /var/log/apache2/access.log | python mypyfw.py
+
+## Need to know
+The IP split line may has an error.
+By default the IP should be located at cell 0. If you use combined logging than it may is 1. 
--- mypyfw.py
+++ mypyfw.py
@@ -6,17 +6,18 @@
 import iptc
 import sys
 import re
+import datetime
 
 recent = list()
 for line in sys.stdin:
-    IP = line.split()[0]
+    IP = line.split()[1] # May need to be adjust, default 0 should work, combined is 1
     Client = line.split('"')[-2]
-    m = re.search('Wget|Python|sqlmap',Client) # related services
-    i = re.search('127.0.0.1',IP) # Whitelabeld IP's
+    m = re.search('Wget|Python|sqlmap|curl',Client) # related services
+    i = re.search('127.0.0.1|87.179.164.194',IP) # Whitelabeld IP's
     if ( m is not None):
         if ( i is None ):
             if not any(IP in s for s in recent):
-                print IP + " " + Client
+                print str(datetime.datetime.now()) + " " +IP + " Header: " + Client + " Matched Rule: " + str(m.group(0))
                 recent.insert(0, IP)
                 rule = iptc.Rule()
                 rule.in_interface = "eth0"
