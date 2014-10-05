1baefe24e9cc54612cc5844bfc96ab085f18cd86
root <root@johest.de>
2014-10-05 16:03:31 +0200

    cleanup script

---
+++ Addons/mypy-ipfw-cleanup.py
@@ -0,0 +1,19 @@
+#!usr/bin/python
+
+import time, sys
+import iptc
+
+table = iptc.Table(iptc.Table.FILTER)
+chain = iptc.Chain(table, "INPUT")
+
+try:
+    table.autocommit = False
+    for rule in chain.rules:
+       (packets, bytes) = rule.get_counters()
+       if int(packets) == 0:
+          chain.delete_rule(rule)
+    table.commit()
+    table.close()
+except iptc.ip4tc.IPTCError: 
+   print "Oops!" 
+   
