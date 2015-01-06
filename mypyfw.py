35b9d4c99fbc6fbe9fb08b59c28f5b4ff3c79355
Joerg Stephan <johe.stephan@ymail.com>
2015-01-06 20:24:38 +0100

    Header analyzer into function improved on pf

--- mypyfw.py
+++ mypyfw.py
@@ -11,6 +11,11 @@ import datetime
 from optparse import OptionParser
 from geoip import geolite2
 
+
+
+
+
+
 def iptablesDrop(IP):
     import iptc
     try:
@@ -21,23 +26,23 @@ def iptablesDrop(IP):
 		chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
 		chain.insert_rule(rule)
     except:
-		print "Unexpected error:", sys.exc_info()[0]
+		print "Unexpected error (iptables):", sys.exc_info()[0]
 		return
         
 
 def pfDrop(IP):
     import pf
+    import socket
     filter = pf.PacketFilter()
     blockip = pf.PFAddr(IP)
     try:
-		# Enable packet filtering
 		filter.enable()
 		ext_if = pf.PFAddr(type=pf.PF_ADDR_DYNIFTL, ifname=options.interface)
 		ruling = pf.PFRule(action=pf.PF_DROP,
-               direction=pf.PF_IN,
-               quick=True,
-               src=pf.PFRuleAddr(blockip))
-        # Initialize and populate the ruleset
+               		direction=pf.PF_IN,
+		 	af=socket.AF_INET, 
+               		quick=True,
+               		src=pf.PFRuleAddr(blockip))
 		rs = pf.PFRuleset()
 		rs.append(ruling)
 
@@ -45,7 +50,7 @@ def pfDrop(IP):
 		filter = pf.PacketFilter()
 		filter.load_ruleset(rs)
     except:
-		print "Unexpected error:", sys.exc_info()[0]
+		print "Unexpected error (pf):", sys.exc_info()[0]
 		return
 
 
@@ -64,10 +69,40 @@ def GETanalyzer(request, IP):
 	else:
             return False
     except:
-        print "Unexpected error:", sys.exc_info()[0]
+        print "Unexpected error (GET):", sys.exc_info()[0]
         return False
 
-    
+
+def HEADERanalyzer(line):
+    try:
+	xcounter = 0
+	IP = line.split()[options.IPpos] # May need to be adjust, default 0 should work, combined is 1
+	Request = line.split('"')[1].lower()
+	Client = line.split('"')[-2]
+	logstring = str(datetime.datetime.now()) + " " + IP + " Header: " + Client 
+	m = re.search(blacklist,Client) # related services
+	i = re.search(whitelist,IP) # Whitelabeld IP's
+	if ( m is not None) or ( GETanalyzer(Request,IP) ):
+		logstring += " Matched Rule: " + str(m.group(0)) 
+		if ( i is None ) :
+			if not any(IP in s for s in recent):
+				if options.geoip:
+					match = geolite2.lookup(IP)
+					if match is not None:
+						logstring += " Country: " + match.country
+			print logstring
+			xcounter += 1
+    			if  options.tryrun:
+				recent.append(IP)
+			if options.enable_pf:
+				pfDrop(IP)
+			else:
+				iptablesDrop(IP)
+	return xcounter
+    except:
+        print "Unexpected error (Header):", sys.exc_info()[0]
+        return 0
+
 
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename",
@@ -86,13 +121,14 @@ parser.add_option("-p", "--pf", action="store_true", dest="enable_pf", default=F
 		  help="use PF as firewall (ex. on openBSD)")
 parser.add_option("-n", "--net", dest="interface", default="eth0",
 		  help=" set iptables/pf network interface")
+parser.add_option("-s", "--inputstream", dest="streamsource",
+		  help="Apache Log file to follow stream", metavar="FILE")
 
 
 (options, args) = parser.parse_args()
 
 blacklist = "Wget|Python|sqlmap|curl|-|apach0day"
 whitelist = "127.0.0.1|::1"
-
 
 # Parsing Options
 if (options.filename is None): 
@@ -110,35 +146,22 @@ if (options.whitelist is not None):
     for line in open(options.whitelist, "r") :
         whitelist = whitelist + "|" + line.rstrip()
     print "extended Whitelist: " + whitelist
-        
+
 logf = open(options.filename,'a')
 sys.stdout = logf
 recent = list()
 counter = 0
+        
+if (options.streamsource is not None):
+	source = open(options.streamsource,'r')
+else:
+	source = sys.stdin
 
-for line in sys.stdin:
-    IP = line.split()[options.IPpos] # May need to be adjust, default 0 should work, combined is 1
-    Request = line.split('"')[1].lower()
-    Client = line.split('"')[-2]
-    logstring = str(datetime.datetime.now()) + " " + IP + " Header: " + Client 
-    m = re.search(blacklist,Client) # related services
-    i = re.search(whitelist,IP) # Whitelabeld IP's
-    if ( m is not None) or ( GETanalyzer(Request,IP) ):
-        logstring += " Matched Rule: " + str(m.group(0)) 
-        if ( i is None ) :
-            if not any(IP in s for s in recent):
-                if options.geoip:
-                    match = geolite2.lookup(IP)
-                    if match is not None:
-                        logstring += " Country: " + match.country
-                print logstring
-		counter += 1
-              	if  options.tryrun:
-		    recent.append(IP)
-                    if options.enable_pf:
-			pfDrop(IP)
-		    else:
-			iptablesDrop(IP)
+for line in source:
+    counter += HEADERanalyzer(line)
+    
+    
+    
 logf.close()
 sys.stdout = sys.__stdout__
 
